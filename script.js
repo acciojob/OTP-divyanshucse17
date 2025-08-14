7a6c8ca54a6ca34c5c7eee79363deb26e6fed704
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-14 09:36:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus(); // Focus first input on page load
+
+codes.forEach((code, idx) => {
+  code.addEventListener('input', (e) => {
+    const value = e.target.value;
+    if (value && idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
+  });
+
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (e.target.value === '' && idx > 0) {
+        codes[idx - 1].focus();
+        codes[idx - 1].value = '';
+      }
+    }
+  });
+});
